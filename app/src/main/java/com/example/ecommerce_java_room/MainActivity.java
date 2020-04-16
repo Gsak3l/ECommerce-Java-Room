2dc6735535192358a46ac2a8cfec9e1cc35f4b46
gsak3l <gsakel5@gmail.com>
2020-04-16 11:08:24 +0300

    main activity transfers us to the login activity

--- app/src/main/java/com/example/ecommerce_java_room/MainActivity.java
+++ app/src/main/java/com/example/ecommerce_java_room/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ecommerce_java_room;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         //giving the values to the buttons from our xml
         loginButton = (Button) findViewById(R.id.welcome_login_button);
         signupButton = (Button) findViewById(R.id.welcome_signup_button);
+
+        //when login button of our homepage gets clicked, we change the view from mainactivity to the login activity
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+            }
+        });
 
     }
 }
