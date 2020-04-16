0158e47f545fe1a01153cdc3579ede2d4ed17a75
gsak3l <gsakel5@gmail.com>
2020-04-16 10:48:44 +0300

    login page almost done

--- app/src/main/java/com/example/ecommerce_java_room/MainActivity.java
+++ app/src/main/java/com/example/ecommerce_java_room/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.ecommerce_java_room;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //variables
+    private Button loginButton;
+    private Button signupButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //giving the values to the buttons from our xml
+        loginButton = (Button) findViewById(R.id.welcome_login_button);
+        signupButton = (Button) findViewById(R.id.welcome_signup_button);
+
     }
 }
