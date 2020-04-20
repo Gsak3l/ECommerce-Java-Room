a40f1c87b2879be1dfc688af1f5144bb2ca4722b
gsak3l <gsakel5@gmail.com>
2020-04-20 12:09:55 +0300

    many many things added

--- app/src/main/java/com/example/ecommerce_java_room/RegisterActivity.java
+++ app/src/main/java/com/example/ecommerce_java_room/RegisterActivity.java
@@ -3,12 +3,24 @@ package com.example.ecommerce_java_room;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
+    private EditText registerEmail;
+    private EditText registerPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //getting all the elements from our xml file
+        createAccountButton = (Button) findViewById(R.id.register_button);
+        registerEmail = (EditText) findViewById(R.id.register_email);
+        registerPassword = (EditText) findViewById(R.id.register_password);
+
     }
 }
