65d58b63c9235d53e0ae089e50a595c41d860cdf
gsak3l <gsakel5@gmail.com>
2020-04-26 20:28:27 +0300

    material spinner for item categories works

--- app/src/main/java/com/example/ecommerce_java_room/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/ecommerce_java_room/AdminAddNewProductActivity.java
@@ -3,16 +3,57 @@ package com.example.ecommerce_java_room;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import fr.ganfra.materialspinner.MaterialSpinner;
+import com.google.android.material.snackbar.Snackbar;
+import com.jaredrummler.materialspinner.MaterialSpinner;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
-    MaterialSpinner product_category_spinner;
+
+    MaterialSpinner productTypeSpinner;
+    TextView live_tester;
+    List<String> typesAvailable = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_new_product);
-        product_category_spinner = (MaterialSpinner) findViewById(R.id.spinner);
+
+        //adding available categories to our product spinner
+        typesAvailable.add("T Shirts for Men");
+        typesAvailable.add("Trousers for Men");
+        typesAvailable.add("Sweaters for Men");
+        typesAvailable.add("Shoes for Men");
+        typesAvailable.add("Dresses for Women");
+        typesAvailable.add("Trousers for Women");
+        typesAvailable.add("Skirts for Women");
+        typesAvailable.add("Shoes for Women");
+        typesAvailable.add("Hats");
+        typesAvailable.add("Purses");
+        typesAvailable.add("Watches");
+        typesAvailable.add("Scarfs");
+        productTypeSpinner = findViewById(R.id.add_new_product_category);
+        live_tester = findViewById(R.id.live_tester);
+        ArrayAdapter products = new ArrayAdapter(this, android.R.layout.simple_spinner_item, typesAvailable);
+        products.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        productTypeSpinner.setAdapter(products);
+
+        productTypeSpinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                live_tester.setText(item);
+            }
+        });
+
+
     }
 }
