bca31c3e33d08a69ce538adb448b7b232731787b
gsak3l <gsakel5@gmail.com>
2020-05-12 21:11:34 +0300

    purchase button works

--- app/src/main/java/com/example/ecommerce_java_room/UserProductBuy.java
+++ app/src/main/java/com/example/ecommerce_java_room/UserProductBuy.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.ecommerce_java_room.data.OrderDAO;
+import com.example.ecommerce_java_room.data.OrderDatabase;
 import com.example.ecommerce_java_room.data.ProductDAO;
 import com.example.ecommerce_java_room.data.ProductDatabase;
 import com.example.ecommerce_java_room.data.UserDAO;
@@ -37,11 +42,13 @@ public class UserProductBuy extends AppCompatActivity {
     private ImageView productImage;
     private TextView productPrice;
     private NumberPicker productQuantity;
+    private Button buyProductButton;
     //minimizing the decimal length of a double
     DecimalFormat df = new DecimalFormat();
     //database stuff
     private ProductDAO productDAO;
     private UserDAO userDAO;
+    private OrderDAO orderDAO;
     //drawer stuff
     private Toolbar toolbar;
     private PrimaryDrawerItem homeNav;
@@ -66,11 +73,14 @@ public class UserProductBuy extends AppCompatActivity {
                 .allowMainThreadQueries().build().getProductDao();
         userDAO = Room.databaseBuilder(this, UserDatabase.class, "User").
                 allowMainThreadQueries().build().getUserDao();
+        orderDAO = Room.databaseBuilder(this, OrderDatabase.class, "Order")
+                .allowMainThreadQueries().build().getOrderDao();
         //finding the xml elements by id
         productTitle = findViewById(R.id.user_buy_product_title);
         productImage = findViewById(R.id.user_buy_product_image);
         productPrice = findViewById(R.id.user_buy_product_price);
         productQuantity = findViewById(R.id.user_buy_product_quantity);
+        buyProductButton = findViewById(R.id.user_buy_add_to_card);
         Product product = productDAO.getProduct(productId);
         //table that contains all the numbers for the numberPicker
         //giving values to the fields
@@ -82,8 +92,14 @@ public class UserProductBuy extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar_buy);
         //setting up the tool bar
         setToolbar();
-        //Toast.makeText(UserProductBuy.this, "i am alive you son of a bitch: " + id, Toast.LENGTH_SHORT).show();
+        //action for the onclick add purchase button
+        buyProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //getting all the values for price, quantity, etc...
 
+            }
+        });
     }
 
     public void setToolbar() {
